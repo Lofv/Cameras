fb7ea084d81fde73f4c3c3cd3283b2aa463647f8
Lofv <dredintellekt@bk.ru>
2016-11-28 17:12:40 +1000

    s

--- src/main/Controller/Podryad.java
+++ src/main/Controller/Podryad.java
@@ -1,4 +1,3 @@
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -6,50 +5,68 @@ import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by Lofv on 25.11.2016.
  */
 public class Podryad {
+    final static String url = "http://live.podryad.tv/";
+    static ArrayList<String> imgLinks = new ArrayList<String>();
+    static ArrayList<String> videoLinks = new ArrayList<String>();
+    static ArrayList<String> nameOfCameras = new ArrayList<String>();
+    static String imgLink;
+    static String videoLink;
+    static String nameOfCamera;
+    static By imgSource = By.cssSelector("img.camera-overlay");
+    static By videoSource = By.cssSelector("a.navbar-brand");
+    static By classOfCamers = By.cssSelector("div.camera.col-lg-4.col-md-6.col-sm-12.col-xs-12");
 
 
-    public void podryadCamers() {
+    public static void podryadCamers() {
 
-        File src =new File("D:/phantomjs/bin/phantomjs.exe");
-        System.setProperty("phantomjs.binary.path",src.getAbsolutePath());
-
+//        File src = new File("D:/phantomjs/bin/phantomjs.exe");
+        File src = new File("C:/Develops/phantomjs/bin/phantomjs.exe");
+        System.setProperty("phantomjs.binary.path", src.getAbsolutePath());
 
         WebDriver driver = new PhantomJSDriver();
-        driver.get("http://live.podryad.tv/vsaas/cameras");
+        driver.get(url+"vsaas/cameras");
 
-
-        WebDriverWait wait = new WebDriverWait(driver, 2);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.camera-list-container")));
 
-
-        By classOfCamers=By.cssSelector("div.camera.col-lg-4.col-md-6.col-sm-12.col-xs-12");
-        List<WebElement> webElements= driver.findElements(classOfCamers);
+        List<WebElement> webElements = driver.findElements(classOfCamers);
 
         for (WebElement webElement : webElements) {
 
+            imgLink = webElement.findElement(imgSource).getAttribute("src");  //ссылка на изображение, которое генерируется для камер каждые пару секунд
+            videoLink = webElement.findElement(videoSource).getAttribute("href");//ссылка на камеру
+            nameOfCamera = webElement.findElement(videoSource).getText();//названия камер
 
 
-
-            By imgSource=By.cssSelector("img.camera-overlay");
-            By videoSource=By.cssSelector("a.navbar-brand");
-
-            String imgLink= webElement.findElement(imgSource).getAttribute("src");  //ссылка на изображение, которое генерируется для камер каждые пару секунд
-            String videoLink=webElement.findElement(videoSource).getAttribute("href");//ссылка на камеру
-            String nameOfCamera=webElement.findElement(videoSource).getText();//названия камер
-
-            System.out.println(nameOfCamera);
-            System.out.println(imgLink);
-            System.out.println(videoLink);
-
+            imgLinks.add(imgLink);
+            videoLinks.add(videoLink);
+            nameOfCameras.add(nameOfCamera);
 
         }
+
+        driver.close(); // после отладки, надо будет перенести это в finally  JavaFX. Чтобы если вдруг будет ошибка, то соединение все равно закрывалось и не жрало память
     }
+
+
+    public static ArrayList<String> getNameOfCameras() {
+        return nameOfCameras;
+    }
+
+    public static ArrayList<String> getImgLinks() {
+        return imgLinks;
+    }
+
+    public static ArrayList<String> getVideoLinks() {
+        return videoLinks;
+    }
+
+
 }
